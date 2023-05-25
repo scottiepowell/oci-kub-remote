a3be583ebc27681155124783cd240ac208d43bad
Scott Powell <scottiepowell@gmail.com>
2023-05-25 08:25:52 -0600

    readme.md

--- variables.tf
+++ variables.tf
@@ -5,12 +5,10 @@ variable "region" {
 variable "compartment_id" {
   type        = string
   description = "The compartment to create the resources in"
-  #default="ocid1.tenancy.oc1..aaaaaaaao6x7uwzfuasfh5ia5qz4zhw4j7oxl6hkhxq4vc7vdqchyxde3una"
 }
 variable "public_subnet_id" {
   type = string
   description = "The public subnet's OCID"
-  default ="ocid1.subnet.oc1.phx.aaaaaaaa3ir55oy6lbkn2n4j4txltw7lezer37ghdihdz6shkgnrdtevvjoq"
 }
 variable "node_pool_id" {
   type = string
